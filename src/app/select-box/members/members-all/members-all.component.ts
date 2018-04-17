402fdfa62a1832d1549b456a0b99cdc5ecf31ea7
Janna95 <jannahambardzumyan95@gmail.com>
2018-04-17 18:22:46 +0400

    something

--- src/app/select-box/members/members-all/members-all.component.ts
+++ src/app/select-box/members/members-all/members-all.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-members-all',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembersAllComponent {
 
-  constructor() { }
+ 
 
  
 }
