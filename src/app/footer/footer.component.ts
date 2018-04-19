7c53531b46c57be8ddc37a1b85ba0907777f2d1d
Arthur <arthuraleqsanyan@gmail.com>
2018-04-19 15:30:50 +0400

    for Zhanna

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,16 @@
-import {Component} from "@angular/core";
-import {NavigationComponent} from ".././navigation/navigation.component";
+import {Component} from '@angular/core';
+import {NavigationComponent} from '../navigation/navigation.component';
 
 
 @Component({
-    selector:'app-footer',
+    selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent  {
-    public nav:Object=new NavigationComponent();
-    navItems=this.nav['navItems'];
+    public nav: Object = new NavigationComponent();
+    navItems = this.nav['navItems'];
 
-}
+
+  public year: number = 2018;
+  }
